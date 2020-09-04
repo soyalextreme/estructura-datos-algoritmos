46d452b0641459e37ed31c792664a5f61ca712ca
Alejandro Andrade <alex.anso.17@gmail.com>
2020-09-04 18:06:30 -0500

    Empezando las clases de Estructuras de Datos y Algoritmos

---
+++ scripts/hello_world.py
@@ -0,0 +1,15 @@
+
+
+def welcome_on_name(name="Fulanito"):
+    """ Says hello depending on a name """ 
+    print(f"Saludos a {name}")
+    a = 10
+    b = 20
+    game_over = False
+    name = "Alejandro"
+    print(type(a))
+    print(type(game_over), game_over)
+    
+
+if __name__ == "__main__":
+    welcome_on_name("Alejandro")
