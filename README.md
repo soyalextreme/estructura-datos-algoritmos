e2b570c6ba655e5099d3efa79ef5ceb41622bf93
Alejandro Andrade <alex.anso.17@gmail.com>
2020-09-04 19:10:15 -0500

    Terminando la segunda sesion.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Estructura de Datos y Algoritmos
+
+#### 6to semestre
+
+Apuntes, Scripts y proyecto de mi materia de Estructura de Datos y algoritmos de 6to Semestre.
+
+Alejandro Andrade Soriano
---
+++ scripts/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+void printSomething(string something){
+    cout << something << endl;
+}
+
+
+int main()
+{
+    printSomething("Hola mundo desde c++");
+    return 0;
+}
